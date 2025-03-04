bed1dcd46213c0e734222273b0a8a6e14f5eafaf
roshan.bangera@spryiq.co <roshan.bangera@spryiq.co>
2025-03-04 11:51:38 +0530

    Added following Files: 1) connector.py 2) constants.py 3) info.json 4) microsoft_api_auth.py 5) operations.py AND ADDED SAMPLE playbooks.json AND IMAGES

---
+++ microsoft-defender-threat-intelligence/constants.py
@@ -0,0 +1,19 @@
+"""
+Copyright start
+MIT License
+Copyright (c) 2025 Fortinet Inc
+Copyright end
+"""
+RESOURCE = "https://graph.microsoft.com"
+SCOPE = 'https://graph.microsoft.com/.default'
+API_VERSION = 'v1.0'
+AUTH_USING_APP = "Without a User - Application Permission"
+AUTH_BEHALF_OF_USER = "On behalf of User - Delegate Permission"
+REFRESH_TOKEN_FLAG = False
+DEFAULT_REDIRECT_URL = 'https://localhost/myapp'
+AUTH_URL = 'https://login.microsoftonline.com'
+# grant types
+CLIENT_CREDENTIALS = 'client_credentials'
+AUTHORIZATION_CODE = 'authorization_code'
+REFRESH_TOKEN = 'refresh_token'
+CERTIFICATE_BASED_AUTH_TYPE = "Certificate Based Authentication"
